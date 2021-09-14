54e5184defcf70ff5875e82b87998855c9614f57
Pichau <diogo.sucupira01@outlook.com>
2021-09-13 22:03:45 -0300

    Add Categorias

---
+++ routes/Categoria.js
@@ -0,0 +1,41 @@
+//API REST de Categorias
+const express = require('express')
+const router = express.Router()
+
+const Categoria = require('../model/Categoria')
+
+
+/* 
+    GET /categoria
+    Lista todas as categorias
+*/
+
+router.get('/', async(req, res) => {
+    try{
+        const categorias = await Categoria.find()
+        res.json(categorias)
+    }catch (err){
+        res.status(500).send({
+            errors: [{message: 'Não foi possível obter as categorias!'}]
+        })
+    }
+})
+
+
+/* 
+    POST /categoria
+    Inclui todas as categorias
+*/
+router.post('/', async(req, res) => {
+    try{
+        let categoria = new Categoria(req.body)
+        await categoria.save()
+        res.send(categoria)
+    }catch (err){
+        return res.status(500).json({
+            errors: [{message: `Erro ao salvar a categoria: ${err.message}`}]
+        })
+    }
+})
+
+module.exports = router
